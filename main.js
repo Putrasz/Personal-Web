5b82744a3873a86133ab0472902c2d276fa9954c
putraaajw <hafidhgpf03@gmail.com>
2025-01-19 07:17:05 +0700

    almost done

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   navbarLinks.classList.toggle("active");
   hamburger.classList.toggle("active");
 }
+
+const thumbnails = document.querySelectorAll(".thumbnail");
+const fullscreen = document.getElementById("fullscreen");
+const fullscreenImg = document.getElementById("fullscreen-img");
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", () => {
+    const fullImageUrl = thumbnail.getAttribute("data-full");
+    fullscreenImg.src = fullImageUrl;
+    fullscreen.style.display = "flex";
+  });
+});
+
+fullscreen.addEventListener("click", () => {
+  fullscreen.style.display = "none";
+});
